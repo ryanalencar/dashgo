b6e2cd83a00d524e749a1436f145fe9e205f1c36
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-01 17:58:58 -0300

    feat: dashboard page with chart

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,44 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const options = {
+  chart: {
+    toolbar: { show: false },
+    zoom: { enabled: false },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: {
+    theme: "dark",
+    x: { show: false },
+  },
+  xaxix: {
+    type: "numeric",
+    axisBorder: { color: theme.colors.gray[600] },
+    axisTicks: { color: theme.colors.gray[600] },
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+};
+const series = [
+  {
+    name: "inscritos",
+    data: [31, 120, 19, 150, 123, 124, 50],
+  },
+];
 
 export default function DashBoard() {
   return (
@@ -9,6 +47,26 @@ export default function DashBoard() {
 
       <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
+
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          style={{ alignContent: "flex-start" }}
+        >
+          <Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
