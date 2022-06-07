9e805e6538383270c4a8bf9f579a46b5b330973d
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-07 10:37:07 -0300

    feat: responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface IProfileProps {
+  showProfileData?: boolean;
+}
+export default function Profile({ showProfileData = true }): IProfileProps {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Ryan Alencar</Text>
-        <Text color="gray.300" fontSize="small">
-          ryanalencarbarbosa1701@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Ryan Alencar</Text>
+          <Text color="gray.300" fontSize="small">
+            ryanalencarbarbosa1701@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
