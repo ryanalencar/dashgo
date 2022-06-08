2b20c6399b89fa65b2d660bd9653cf2652bf02c2
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-08 09:41:41 -0300

    feat: sidebar drawer context

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,24 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { createContext, ReactNode, useContext } from "react";
+
+interface ISidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({
+  children,
+}: ISidebarDrawerProviderProps) {
+  const disclosure = useDisclosure();
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
