4fdec51546e937c3ebda518202c86097cf4fea2c
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-08 09:41:10 -0300

    feat: sidebar nav component

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "@chakra-ui/react";
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+
+import NavItem from "./NavItem";
+import NavSection from "./NavSection";
+
+export default function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="geral">
+        <NavItem href="/dashboard" icon={RiDashboardLine} text="dashboard" />
+        <NavItem href="/users" icon={RiContactsLine} text="usuários" />
+      </NavSection>
+      <NavSection title="automação">
+        <NavItem href="" icon={RiInputMethodLine} text="formulários" />
+        <NavItem href="" icon={RiGitMergeLine} text="automação" />
+      </NavSection>
+    </Stack>
+  );
+}
