861c3c2bc005363f3cadcaa3ff4cf4cba6852c47
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-15 11:45:28 -0300

    feat: pagination component logic

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,8 +1,45 @@
-import { Box, Button, HStack, Stack } from "@chakra-ui/react";
+import { Box, Button, HStack, Stack, Text } from "@chakra-ui/react";
 
 import PaginationItem from "./PaginationItem";
 
-export default function Pagination() {
+interface IPaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const SIBLINGS_COUNT = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export default function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: IPaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPages =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - SIBLINGS_COUNT, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + SIBLINGS_COUNT, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -16,9 +53,39 @@ export default function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <HStack spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
+        {currentPage > 1 + SIBLINGS_COUNT && (
+          <>
+            <PaginationItem number={1} />
+            {currentPage > 2 + SIBLINGS_COUNT && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+          </>
+        )}
+
+        {previousPages.length > 0 &&
+          previousPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        <PaginationItem number={currentPage} isCurrent />
+
+        {nextPages.length > 0 &&
+          nextPages.map((page) => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+
+        {currentPage + SIBLINGS_COUNT < lastPage && (
+          <>
+            {currentPage + 1 + SIBLINGS_COUNT < lastPage && (
+              <Text color="gray.300" w="8" textAlign="center">
+                ...
+              </Text>
+            )}
+            <PaginationItem number={lastPage} />
+          </>
+        )}
       </HStack>
     </Stack>
   );
