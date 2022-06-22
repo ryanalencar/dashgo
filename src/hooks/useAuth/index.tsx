d5af24e41cf7ab697d98f9ab82e1e3bc241648f0
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-22 16:00:27 -0300

    feat: saving tokens on cookie

--- src/hooks/useAuth/index.tsx
+++ src/hooks/useAuth/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { createContext, useContext, useState } from "react";
-
+import { setCookie } from "nookies";
 import { _api } from "../../services/api";
 import {
   AuthContextData,
@@ -19,8 +19,18 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const signIn = async (data: SignInCredentials) => {
     try {
       const response = await _api.post("sessions", data);
-      const { permissions, roles, email } = response.data;
+      const { permissions, roles, email, token, refreshToken } = response.data;
+
       setUser({ email, permissions, roles });
+      setCookie(undefined, "dashgo.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+      setCookie(undefined, "dashgo.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
       router.push("/dashboard");
     } catch (error) {
       console.error(error);
