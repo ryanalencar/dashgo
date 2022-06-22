6b8774dd7996ebd729cf2b90b0636ff3e3c5d633
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-06-22 16:20:45 -0300

    feat: recovering user token info

--- src/hooks/useAuth/index.tsx
+++ src/hooks/useAuth/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
-import { createContext, useContext, useState } from "react";
-import { setCookie } from "nookies";
+import { createContext, useContext, useEffect, useState } from "react";
+import { parseCookies, setCookie } from "nookies";
 import { _api } from "../../services/api";
 import {
   AuthContextData,
@@ -16,12 +16,22 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "dashgo.token": token } = parseCookies();
+
+    if (token) {
+      _api.get("me").then((res) => {
+        const { email, permissions, roles } = res.data;
+        setUser({ email, permissions, roles });
+      });
+    }
+  }, []);
+
   const signIn = async (data: SignInCredentials) => {
     try {
       const response = await _api.post("sessions", data);
       const { permissions, roles, email, token, refreshToken } = response.data;
 
-      setUser({ email, permissions, roles });
       setCookie(undefined, "dashgo.token", token, {
         maxAge: 60 * 60 * 24 * 30,
         path: "/",
@@ -31,6 +41,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
         path: "/",
       });
 
+      setUser({ email, permissions, roles });
+
+      _api.defaults.headers["Authorization"] = `Bearer ${token}`;
       router.push("/dashboard");
     } catch (error) {
       console.error(error);
